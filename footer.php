b647b426c2e1e8557693c6c9aa4db634d92178be
syifaKamalia <syfakml2@gmail.com>
2023-05-31 12:37:48 +0800

    selesai pertemuan 3

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div>
+<footer>
+    <div class="container"> <span>&copy; <?php echo date('Y'); ?> - FTI UNISKA</span>
+    </div>
+</footer>
+<script src="js/jquery-3.5.1.slim.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ profile.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/syifa.jpg" alt="Syifa Kamalia" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIM :</strong></td>
+                        <td align="left" width="50%">2110010171</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Syifa Kamalia</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 21 Oktober 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0887 5036 138</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">syfakml2@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://instagram.com/syifakmalia" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://t.me/Syfakmalia" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/telegram.svg" width="30" alt="telegram">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
